b872e7cc6ad6cfbebb460b75bb07da5fdeebc7df
Nicolas Widart <n.widart@gmail.com>
2014-11-27 20:22:55 +0100

    Translate authentication messages

---
+++ Resources/lang/en/messages.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'successfully logged in' => 'Successfully logged in',
+    'account created check email for activation' => 'Account created. Please check your email to activate your account.',
+    'account activated you can now login' => 'Account activated. You can now login.',
+    'there was an error with the activation' => 'There was an error with the activation.',
+    'no user found' => 'No user with that email address belongs in our system.',
+    'check email to reset password' => 'Check your email to reset your password.',
+    'user no longer exists' => 'The user no longer exists.',
+    'invalid reset code' => 'Invalid or expired reset code.',
+    'password reset' => 'Password has been reset. You can now login with your new password.',
+];
