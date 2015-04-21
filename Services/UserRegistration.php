9bd00e35e166b2d22e438f1aa7b096c21aeec72c
Nicolas Widart <n.widart@gmail.com>
2015-04-21 16:08:51 +0200

    Add a new user registration service class

---
+++ Services/UserRegistration.php
@@ -0,0 +1,56 @@
+<?php namespace Modules\User\Services;
+
+use Modules\Core\Contracts\Authentication;
+use Modules\User\Events\UserHasRegistered;
+use Modules\User\Repositories\RoleRepository;
+
+class UserRegistration
+{
+    /**
+     * @var Authentication
+     */
+    private $auth;
+    /**
+     * @var RoleRepository
+     */
+    private $role;
+    /**
+     * @var array
+     */
+    private $input;
+
+    public function __construct(Authentication $auth, RoleRepository $role)
+    {
+        $this->auth = $auth;
+        $this->role = $role;
+    }
+
+    /**
+     * @param array $input
+     * @return mixed
+     */
+    public function register(array $input)
+    {
+        $this->input = $input;
+
+        $user = $this->createUser();
+
+        $this->assignUserToUsersGroup($user);
+
+        event(new UserHasRegistered($user));
+
+        return $user;
+    }
+
+    private function createUser()
+    {
+        return $this->auth->register((array) $this->input);
+    }
+
+    private function assignUserToUsersGroup($user)
+    {
+        $role = $this->role->findByName('User');
+
+        $this->auth->assignRole($user, $role);
+    }
+}
