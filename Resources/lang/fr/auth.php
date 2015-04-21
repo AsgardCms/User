e84b0877500bf615ea1ca63742b666442956627c
Nicolas Widart <n.widart@gmail.com>
2015-04-21 13:54:06 +0200

    Adding french translation

---
+++ Resources/lang/fr/auth.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'login' => 'Connexion',
+    'email' => 'Email',
+    'remember me' => 'Se souvenir de moi',
+    'forgot password' => 'J\'ai oublié mon mot de passe',
+    'register' => 'Créer un nouveau compte',
+    'password' => 'Mot de passe',
+    'password confirmation' => 'Confirmation de mot de passe',
+    'register me' => 'S\'enregistrer',
+    'I already have a membership' => 'J\'ai déjà un compte',
+    'reset password' => 'Remise à zéro',
+    'I remembered my password' => 'Je me souvient de mon mot de passe',
+];
