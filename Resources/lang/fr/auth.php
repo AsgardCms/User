336f03a98fa0b7bf29a42c6f97f345bccaa27b13
Nicolas Widart <n.widart@gmail.com>
2015-04-22 09:25:58 +0200

    Translating to french

--- Resources/lang/fr/auth.php
+++ Resources/lang/fr/auth.php
@@ -12,4 +12,10 @@ return [
     'I already have a membership' => 'J\'ai déjà un compte',
     'reset password' => 'Remise à zéro',
     'I remembered my password' => 'Je me souvient de mon mot de passe',
+    'to reset password complete this form' => 'Remplissez ce formulaire pour remettre votre mot de passe à zéro',
+    'welcome title' => 'Bienvenue',
+    'please confirm email' => 'Pour remettre votre mot de passe à zéro, cliquez sur le lien suivant: ',
+    'additional confirm email message' => 'Nous pouvons avoir besoin de votre adresse email pour vous envoyer des informations critiques, c\'est pourquoi avoir une adresse correcte est important.',
+    'confirm email button' => 'Confirmez votre adresse email',
+    'end greeting' => 'L\'équipe',
 ];
