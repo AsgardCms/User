1cda26719fae457d1b29b5479b312f7d4927cca8
Clemir <clemir@gmail.com>
2015-07-29 17:59:56 -0430

    Adding Spanish traslation

---
+++ Resources/lang/es/roles.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+    'button' => [
+        'new-role' => 'Nuevo rol',
+    ],
+    'title' => [
+        'roles' => 'Roles',
+        'edit' => 'Editar rol',
+        'users-with-roles' => 'Usuarios con este rol',
+    ],
+    'breadcrumb' => [
+        'roles' => 'Roles',
+        'new' => 'Nuevo',
+        'edit' => 'Editar rol',
+    ],
+    'table' => [
+        'name' => 'Nombre',
+    ],
+    'tabs' => [
+        'data' => 'Datos',
+        'permissions' => 'Permisos',
+    ],
+    'form' => [
+        'name' => 'Nombre del rol',
+        'slug' => 'URL amigable del rol',
+    ],
+    'navigation' => [
+        'back to index' => 'Regresar'
+    ],
+    'select all' => 'Seleccionar todos',
+    'deselect all' => 'Deseleccionar todos',
+    'swap' => 'Cambiar',
+];
