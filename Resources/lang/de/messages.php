1b54f4aadc5f7318a5fac48f50bfe1144bf242a1
Nuno Ribeiro <nmpribeiro@gmail.com>
2015-08-20 15:24:28 +0000

    Added de locale and pt

---
+++ Resources/lang/de/messages.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    /* Authentication */
+    'successfully logged in' => 'Erfolgreich angemeldet',
+    'account created check email for activation' => 'Konto erstellt. Bitte überprüfen Sie Ihre E-Mail an Ihr Konto zu aktivieren.',
+    'account activated you can now login' => 'Konto aktiviert. Sie können jetzt anmelden.',
+    'there was an error with the activation' => 'Benutzername bereits aktiviert. Sie können bereits mit Ihren Anmeldeinformationen anmelden.',
+    'no user found' => 'Kein Benutzer mit dieser E-Mail-Adresse gehört in unserem System.',
+    'check email to reset password' => 'Überprüfen Sie Ihre E-Mail, um Ihr Passwort zurückzusetzen.',
+    'user no longer exists' => 'Der Benutzer nicht mehr existiert.',
+    'invalid reset code' => 'Ungültige oder abgelaufene Rücksetzcode.',
+    'password reset' => 'Passwort zurückgesetzt wurde. Sie können nun mit Ihrem neuen Passwort anmelden.',
+    /* User management */
+    'user created' => 'Benutzer erfolgreich erstellt.',
+    'user not found' => 'Benutzer nicht gefunden.',
+    'user updated' => 'Benutzer erfolgreich aktualisiert.',
+    'user deleted' => 'Benutzer erfolgreich gelöscht.',
+    /* Role management */
+    'role created' => 'Rolle erfolgreich erstellt.',
+    'role not found' => 'Rolle nicht gefunden.',
+    'role updated' => 'Rolle erfolgreich aktualisiert.',
+    'role deleted' => 'Rolle erfolgreich gelöscht.',
+];
