df314e6e8bbe259c5e5eccfbb14170a66e3555cd
Nicolas Widart <n.widart@gmail.com>
2015-11-15 17:16:40 +0100

    Use the route helpers and use short array syntax consistently

--- Http/frontendRoutes.php
+++ Http/frontendRoutes.php
@@ -1,21 +1,19 @@
 <?php
 
-use Illuminate\Routing\Router;
-
-$router->group(['prefix' => 'auth'], function (Router $router) {
+$router->group(['prefix' => 'auth'], function () {
     # Login
-    $router->get('login', ['middleware' => 'auth.guest', 'as' => 'login', 'uses' => 'AuthController@getLogin']);
-    $router->post('login', array('as' => 'login.post', 'uses' => 'AuthController@postLogin'));
+    get('login', ['middleware' => 'auth.guest', 'as' => 'login', 'uses' => 'AuthController@getLogin']);
+    post('login', ['as' => 'login.post', 'uses' => 'AuthController@postLogin']);
     # Register
-    $router->get('register', ['middleware' => 'auth.guest', 'as' => 'register', 'uses' => 'AuthController@getRegister']);
-    $router->post('register', array('as' => 'register.post', 'uses' => 'AuthController@postRegister'));
+    get('register', ['middleware' => 'auth.guest', 'as' => 'register', 'uses' => 'AuthController@getRegister']);
+    post('register', ['as' => 'register.post', 'uses' => 'AuthController@postRegister']);
     # Account Activation
-    $router->get('activate/{userId}/{activationCode}', 'AuthController@getActivate');
+    get('activate/{userId}/{activationCode}', 'AuthController@getActivate');
     # Reset password
-    $router->get('reset', ['as' => 'reset', 'uses' => 'AuthController@getReset']);
-    $router->post('reset', ['as' => 'reset.post', 'uses' => 'AuthController@postReset']);
-    $router->get('reset/{id}/{code}', ['as' => 'reset.complete', 'uses' => 'AuthController@getResetComplete']);
-    $router->post('reset/{id}/{code}', ['as' => 'reset.complete.post', 'uses' => 'AuthController@postResetComplete']);
+    get('reset', ['as' => 'reset', 'uses' => 'AuthController@getReset']);
+    post('reset', ['as' => 'reset.post', 'uses' => 'AuthController@postReset']);
+    get('reset/{id}/{code}', ['as' => 'reset.complete', 'uses' => 'AuthController@getResetComplete']);
+    post('reset/{id}/{code}', ['as' => 'reset.complete.post', 'uses' => 'AuthController@postResetComplete']);
     # Logout
-    $router->get('logout', array('as' => 'logout', 'uses' => 'AuthController@getLogout'));
+    get('logout', ['as' => 'logout', 'uses' => 'AuthController@getLogout']);
 });
