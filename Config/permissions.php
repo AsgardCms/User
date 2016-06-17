ce9082cbb6cd48356b57b0174951fc3de0abeef0
Nicolas Widart <n.widart@gmail.com>
2016-06-17 20:47:55 +0200

    Use new authorize middleware and cleanup permissions

--- Config/permissions.php
+++ Config/permissions.php
@@ -3,18 +3,13 @@ return [
     'user.users' => [
         'index',
         'create',
-        'store',
         'edit',
-        'update',
         'destroy',
-        'sendResetPassword',
     ],
     'user.roles' => [
         'index',
         'create',
-        'store',
         'edit',
-        'update',
         'destroy',
     ],
 ];
--- Http/backendRoutes.php
+++ Http/backendRoutes.php
@@ -4,13 +4,41 @@ use Illuminate\Routing\Router;
 /** @var Router $router */
 
 $router->group(['prefix' => '/user'], function (Router $router) {
-    $router->get('users', ['as' => 'admin.user.user.index', 'uses' => 'UserController@index']);
-    $router->get('users/create', ['as' => 'admin.user.user.create', 'uses' => 'UserController@create']);
-    $router->post('users', ['as' => 'admin.user.user.store', 'uses' => 'UserController@store']);
-    $router->get('users/{users}/edit', ['as' => 'admin.user.user.edit', 'uses' => 'UserController@edit']);
-    $router->put('users/{users}/edit', ['as' => 'admin.user.user.update', 'uses' => 'UserController@update']);
-    $router->get('users/{users}/sendResetPassword', ['as' => 'admin.user.user.sendResetPassword', 'uses' => 'UserController@sendResetPassword']);
-    $router->delete('users/{users}', ['as' => 'admin.user.user.destroy', 'uses' => 'UserController@destroy']);
+    $router->get('users', [
+        'as' => 'admin.user.user.index',
+        'uses' => 'UserController@index',
+        'middleware' => 'can:user.users.index',
+    ]);
+    $router->get('users/create', [
+        'as' => 'admin.user.user.create',
+        'uses' => 'UserController@create',
+        'middleware' => 'can:user.users.create',
+    ]);
+    $router->post('users', [
+        'as' => 'admin.user.user.store',
+        'uses' => 'UserController@store',
+        'middleware' => 'can:user.users.create',
+    ]);
+    $router->get('users/{users}/edit', [
+        'as' => 'admin.user.user.edit',
+        'uses' => 'UserController@edit',
+        'middleware' => 'can:user.users.edit',
+    ]);
+    $router->put('users/{users}/edit', [
+        'as' => 'admin.user.user.update',
+        'uses' => 'UserController@update',
+        'middleware' => 'can:user.users.edit',
+    ]);
+    $router->get('users/{users}/sendResetPassword', [
+        'as' => 'admin.user.user.sendResetPassword',
+        'uses' => 'UserController@sendResetPassword',
+        'middleware' => 'can:user.users.edit',
+    ]);
+    $router->delete('users/{users}', [
+        'as' => 'admin.user.user.destroy',
+        'uses' => 'UserController@destroy',
+        'middleware' => 'can:user.users.destroy',
+    ]);
 
     $router->get('roles', ['as' => 'admin.user.role.index', 'uses' => 'RolesController@index']);
     $router->get('roles/create', ['as' => 'admin.user.role.create', 'uses' => 'RolesController@create']);
