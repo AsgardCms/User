fe8fc60487f24248c02aee3bc894fad68419d6ee
Nicolas Widart <n.widart@gmail.com>
2016-06-25 11:33:10 +0200

    Adapting sentinel group seeder to new permissions namings

--- Database/Seeders/SentinelGroupSeedTableSeeder.php
+++ Database/Seeders/SentinelGroupSeedTableSeeder.php
@@ -37,8 +37,8 @@ class SentinelGroupSeedTableSeeder extends Seeder
         $group = Sentinel::findRoleBySlug('admin');
         $group->permissions = [
             'dashboard.index' => true,
-            'dashboard.grid.save' => true,
-            'dashboard.grid.reset' => true,
+            'dashboard.update' => true,
+            'dashboard.reset' => true,
             /* Workbench */
             'workshop.modules.index' => true,
             'workshop.modules.show' => true,
@@ -49,61 +49,40 @@ class SentinelGroupSeedTableSeeder extends Seeder
             /* Roles */
             'user.roles.index' => true,
             'user.roles.create' => true,
-            'user.roles.store' => true,
             'user.roles.edit' => true,
-            'user.roles.update' => true,
             'user.roles.destroy' => true,
             /* Users */
             'user.users.index' => true,
             'user.users.create' => true,
-            'user.users.store' => true,
             'user.users.edit' => true,
-            'user.users.update' => true,
             'user.users.destroy' => true,
             /* Menu */
             'menu.menus.index' => true,
             'menu.menus.create' => true,
-            'menu.menus.store' => true,
             'menu.menus.edit' => true,
-            'menu.menus.update' => true,
             'menu.menus.destroy' => true,
-            'menu.menuitem.index' => true,
-            'menu.menuitem.create' => true,
-            'menu.menuitem.store' => true,
-            'menu.menuitem.edit' => true,
-            'menu.menuitem.update' => true,
-            'menu.menuitem.destroy' => true,
+            'menu.menuitems.index' => true,
+            'menu.menuitems.create' => true,
+            'menu.menuitems.edit' => true,
+            'menu.menuitems.destroy' => true,
             /* Media */
-            'media.media.index' => true,
-            'media.media.create' => true,
-            'media.media.store' => true,
-            'media.media.edit' => true,
-            'media.media.update' => true,
-            'media.media.destroy' => true,
-            'media.media-grid.index' => true,
-            'media.media-grid.ckIndex' => true,
+            'media.medias.index' => true,
+            'media.medias.create' => true,
+            'media.medias.edit' => true,
+            'media.medias.destroy' => true,
             /* Settings */
             'setting.settings.index' => true,
-            'setting.settings.store' => true,
-            'setting.settings.getModuleSettings' => true,
+            'setting.settings.edit' => true,
             /* Page */
             'page.pages.index' => true,
             'page.pages.create' => true,
-            'page.pages.store' => true,
             'page.pages.edit' => true,
-            'page.pages.update' => true,
             'page.pages.destroy' => true,
             /* Translation */
             'translation.translations.index' => true,
-            'translation.translations.update' => true,
+            'translation.translations.edit' => true,
             'translation.translations.export' => true,
             'translation.translations.import' => true,
-        ];
-        $group->save();
-
-        $group = Sentinel::findRoleBySlug('user');
-        $group->permissions = [
-            'dashboard.index' => true,
         ];
         $group->save();
     }
