233a7aa32c45579cacf9399db49093d38c0ad81a
Nicolas Widart <n.widart@gmail.com>
2016-07-06 21:53:45 +0200

    Adding nicer permission labels

--- Config/permissions.php
+++ Config/permissions.php
@@ -2,20 +2,20 @@
 
 return [
     'user.users' => [
-        'index',
-        'create',
-        'edit',
-        'destroy',
+        'index' => trans('user::users.list user'),
+        'create' => trans('user::users.create user'),
+        'edit' => trans('user::users.edit user'),
+        'destroy' => trans('user::users.destroy user'),
     ],
     'user.roles' => [
-        'index',
-        'create',
-        'edit',
-        'destroy',
+        'index' => trans('user::roles.list resource'),
+        'create' => trans('user::roles.create resource'),
+        'edit' => trans('user::roles.edit resource'),
+        'destroy' => trans('user::roles.destroy resource'),
     ],
     'account.api-keys' => [
-        'index',
-        'create',
-        'destroy',
+        'index' => trans('user::users.list api key'),
+        'create' => trans('user::users.create api key'),
+        'destroy' => trans('user::users.destroy api key'),
     ],
 ];
