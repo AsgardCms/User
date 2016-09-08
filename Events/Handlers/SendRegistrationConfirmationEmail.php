092523657841531866383b67adbfc6661d3aea22
Nicolas Widart <n.widart@gmail.com>
2016-09-08 23:08:28 +0200

    Using welcome email class

--- Events/Handlers/SendRegistrationConfirmationEmail.php
+++ Events/Handlers/SendRegistrationConfirmationEmail.php
@@ -2,10 +2,11 @@
 
 namespace Modules\User\Events\Handlers;
 
-use Illuminate\Mail\Message;
+use Illuminate\Contracts\Mail\Mailer;
 use Illuminate\Support\Facades\Mail;
 use Modules\User\Contracts\Authentication;
 use Modules\User\Events\UserHasRegistered;
+use Modules\User\Emails\WelcomeEmail;
 
 class SendRegistrationConfirmationEmail
 {
@@ -13,10 +14,15 @@ class SendRegistrationConfirmationEmail
      * @var AuthenticationRepository
      */
     private $auth;
+    /**
+     * @var Mailer
+     */
+    private $mail;
 
-    public function __construct(Authentication $auth)
+    public function __construct(Authentication $auth, Mailer $mail)
     {
         $this->auth = $auth;
+        $this->mail = $mail;
     }
 
     public function handle(UserHasRegistered $event)
@@ -25,15 +31,6 @@ class SendRegistrationConfirmationEmail
 
         $activationCode = $this->auth->createActivation($user);
 
-        $data = [
-            'user' => $user,
-            'activationcode' => $activationCode,
-        ];
-
-        Mail::queue('user::emails.welcome', $data,
-            function (Message $m) use ($user) {
-                $m->to($user->email)->subject('Welcome.');
-            }
-        );
+        $this->mail->to($user->email)->send(new WelcomeEmail($user, $activationCode));
     }
 }
